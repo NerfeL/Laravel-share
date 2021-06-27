b9858487b73598ac2df650d3be3b27d46d9b4229
Ivan <nerfel@list.ru>
2021-06-28 01:29:11 +0600

    Added form file auto upload

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 import './bootstrap';
+
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    let fileInput = document.querySelector('form input#input__file');
+    if(fileInput !== null) {
+        fileInput.addEventListener("change", function() {
+            this.parentNode.submit();
+        });
+    }
+
+});
+
